6ab37dc50d7883394d65efc00dde327a7816c5e9
uGustaHd <154080279+uGustaHd@users.noreply.github.com>
2026-05-09 11:00:28 -0300

    Add selectable player movement modes

---
+++ Scripts/player.gd
@@ -0,0 +1,53 @@
+extends CharacterBody2D
+
+enum MovementMode {
+	WASD,
+	ASTEROIDS
+}
+
+@export var movement_mode: MovementMode = MovementMode.WASD
+
+@export_group("WASD Movement")
+@export var move_speed := 300.0
+
+@export_group("Asteroids Movement")
+@export var thrust := 600.0
+@export var turn_speed := 4.0
+@export var max_speed := 500.0
+@export var friction := 0.98
+
+func _physics_process(delta: float) -> void:
+	if movement_mode == MovementMode.WASD:
+		move_wasd()
+	elif movement_mode == MovementMode.ASTEROIDS:
+		move_asteroids(delta)
+
+	move_and_slide()
+
+
+func move_wasd() -> void:
+	var input_direction := Vector2.ZERO
+
+	input_direction.x = Input.get_axis("ui_left", "ui_right")
+	input_direction.y = Input.get_axis("ui_up", "ui_down")
+
+	input_direction = input_direction.normalized()
+
+	velocity = input_direction * move_speed
+
+	look_at(get_global_mouse_position())
+	rotation += deg_to_rad(90)
+
+
+func move_asteroids(delta: float) -> void:
+	var turn_input := Input.get_axis("ui_left", "ui_right")
+	rotation += turn_input * turn_speed * delta
+
+	if Input.is_action_pressed("ui_up"):
+		var direction := Vector2.UP.rotated(rotation)
+		velocity += direction * thrust * delta
+
+	velocity *= friction
+
+	if velocity.length() > max_speed:
+		velocity = velocity.normalized() * max_speed
